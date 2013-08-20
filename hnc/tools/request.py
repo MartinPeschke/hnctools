4f9090fac149ad2e245885bf42dfde04308c12a4
Martin Peschke <martin@per-4.com>
2013-08-20 12:31:01 +0200

    allow traversal based request extension

--- hnc/tools/request.py
+++ hnc/tools/request.py
@@ -12,24 +12,11 @@ class JsonAwareRedirect(Exception):
 def fwd_raw(request, location):
     raise JsonAwareRedirect(location = location)
 
-def rld_url(request, with_query = True, *args, **kwargs):
-    if with_query:
-        return request.current_route_url(_query = request.GET, *args, **kwargs)
-    else:
-        return request.current_route_url(*args, **kwargs)
 
 def rld(request, with_query = True, *args, **kwargs):
     raise JsonAwareRedirect(location = request.rld_url(with_query, *args, **kwargs))
 def fwd(request, route_name, *args, **kwargs):
     raise JsonAwareRedirect(location = request.fwd_url(route_name, *args, **kwargs))
-
-def fwd_url(request, route_name, secure = None, *args, **kwargs):
-    if secure is None:
-        secure = request.scheme == 'https'
-    if secure:
-        return request.route_url(route_name, _scheme = request.globals.secure_scheme, *args, **kwargs)
-    else:
-        return request.route_url(route_name, _scheme = "http", *args, **kwargs)
 
 def ajax_url(request, route_name, secure = False, escaped = {}, *args, **kwargs):
     """
@@ -51,12 +38,17 @@ def jsonAwareRedirectView(exc, request):
     if request.is_xhr:
         response = Response(simplejson.dumps({'redirect': exc.location}), 200, content_type = 'application/json')
     else:
-        response = redirect_response(exc.lcation)
+        response = redirect_response(exc.location)
     return response
 
-    
 
-def extend_request(config):
+
+def extend_request_basic(config):
+    """
+    set up request properties universal for all apps
+    :param config:
+    :return:
+    """
     def furl(request):
         try:
             return request.json_body['furl']
@@ -70,12 +62,53 @@ def extend_request(config):
     def backend(request):
         return request.globals.backend
     config.add_request_method(backend, 'backend', reify=True)
-
     config.add_request_method(fwd_raw)
+
+
+
+def extend_request_dispatch(config):
+    extend_request_basic(config)
+
+    def rld_url(request, with_query = True, *args, **kwargs):
+        if with_query:
+            return request.current_route_url(_query = request.GET, *args, **kwargs)
+        else:
+            return request.current_route_url(*args, **kwargs)
+    def fwd_url(request, route_name, secure = None, *args, **kwargs):
+        if secure is None:
+            secure = request.scheme == 'https'
+        if secure:
+            return request.route_url(route_name, _scheme = request.globals.secure_scheme, *args, **kwargs)
+        else:
+            return request.route_url(route_name, _scheme = "http", *args, **kwargs)
+
     config.add_request_method(rld_url)
     config.add_request_method(rld)
     config.add_request_method(fwd)
     config.add_request_method(fwd_url)
+    config.add_request_method(ajax_url)
+
+    config.add_view(jsonAwareRedirectView, context=JsonAwareRedirect, permission = NO_PERMISSION_REQUIRED)
+
+
+
+def extend_request_traversal(config):
+    extend_request_basic(config)
+
+    def rld_url_traverse(request, *args, **kwargs):
+        return request.resource_url(request.context, *args, **kwargs)
+    def fwd_url_traverse(request, ctxt, secure = None, *args, **kwargs):
+        if secure is None:
+            secure = request.scheme == 'https'
+        if secure:
+            return request.resource_url(ctxt, _scheme = request.globals.secure_scheme, *args, **kwargs)
+        else:
+            return request.resource_url(ctxt, _scheme = "http", *args, **kwargs)
+
+    config.add_request_method(rld_url_traverse, "rld_url")
+    config.add_request_method(rld)
+    config.add_request_method(fwd)
+    config.add_request_method(fwd_url_traverse, name="fwd_url")
     config.add_request_method(ajax_url)
 
     config.add_view(jsonAwareRedirectView, context=JsonAwareRedirect, permission = NO_PERMISSION_REQUIRED)
